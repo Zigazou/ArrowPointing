d1b15df2d5d275c71626b933b047858101cbdc6e
Frédéric BISSON <zigazou@free.fr>
2015-09-24 22:13:34 +0200

    Allow lines of different lengths It thus respects the original challenge

--- src/Grid/Parse.hs
+++ src/Grid/Parse.hs
@@ -12,7 +12,6 @@ module Grid.Parse (toGrid) where
 import Data.Array (listArray)
 import Data.List (transpose)
 import Data.Maybe (fromJust)
-import Control.Arrow ((&&&))
 
 import Cell (Cell (Start), toCell)
 
@@ -24,13 +23,19 @@ Verify a `String` contains a valid problem.
 validateGrid :: String -> Either String String
 validateGrid s
     | null s                          = Left "Empty string"
-    | any ((/= width ls) . length) ls = Left "Rows with different lengths"
     | startCounts s == 0              = Left "No start point"
     | startCounts s > 1               = Left "More than one start point"
     | otherwise                       = Right s
-    where ls = lines s
-          width = length . head
-          startCounts = length . filter (== 'S')
+    where startCounts = length . filter (== 'S')
+
+{- |
+Limit a `String` at a specific length. If the source `String` is less than the
+targetted length, it is padded with spaces.
+-}
+limit :: Int -> String -> String
+limit i s | l >= i = take i s 
+          | otherwise = s ++ replicate (i - l) ' '
+          where l = length s
 
 {- |
 Convert a `String` into a `Grid`.
@@ -39,7 +44,7 @@ toGrid :: String -> Either String Grid
 toGrid s = validateGrid s >> return (Grid start cells)
     where
         ls = lines s
-        (width, height) = (length . head &&& length) ls
+        (width, height) = (maximum (length <$> ls), length ls)
         cells = toCell <$> listArray ((0, 0), (width - 1, height - 1))
-                                     (concat $ transpose ls)
+                                     (concat $ transpose (limit width <$> ls))
         start = fromJust $ arraySearch cells Start
